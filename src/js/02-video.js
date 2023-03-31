7b9303f6852451463e2f613b2b112186c8cf1ce5
NikaDelva <nikadelva@gmail.com>
2023-03-31 19:03:59 +0300

    task 2 started

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player)
+player.on('play', function() {
+        console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+});
+const onPlay = function (data) {
+    cconsole.log(data)
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', onPlay);
