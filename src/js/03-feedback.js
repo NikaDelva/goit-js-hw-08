ed65c80b6c0114407f65d9703e67a9530a348a40
NikaDelva <nikadelva@gmail.com>
2023-04-02 22:39:50 +0300

    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+const userDataStorage = {};
+
+const fillContactForm = () => { 
+    const userInfoLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (userInfoLS === null) {
+        return;
+    }
+    for (const key in userInfoLS) {
+        formEl.elements[key].value = userInfoLS[key];
+    }
+
+};
+fillContactForm()
+
+const onFieldsInput = event => {
+    const { target: onInputValue } = event;
+    const inputFieldValue = onInputValue.value;
+    const inputNameValue = onInputValue.name;
+    userDataStorage[inputNameValue] = inputFieldValue;
+    localStorage.setItem('feedback-form-state', JSON.stringify(userDataStorage))
+    console.log(userDataStorage)
+
+}
+const onFormSumbit = event => {
+    event.preventDefault();
+    formEl.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+formEl.addEventListener('input', throttle(onFieldsInput, 500));
+formEl.addEventListener('submit', onFormSumbit);
 
